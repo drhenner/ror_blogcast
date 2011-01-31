2c6118dac1516fa6181b0017d580d13d5011dd6a
David Henner <davidhenner@drhenner.local>
2011-01-31 07:10:23 +0000

    spec helper methods

---
+++ spec/support/blogcast_test_helpers.rb
@@ -0,0 +1,34 @@
+module Blogcast
+  module TestHelpers
+
+    def login_as(user)
+      #activate_authlogic
+      user_session_for user
+
+      #u ||= Factory(user)
+      @controller.stubs(:current_user).returns(user)
+      #u
+    end
+
+    def user_session_for(user)
+      UserSession.create(user)
+    end
+
+    #def current_user
+    #  UserSession.find.user
+    #end
+
+    def set_current_user(user = Factory(:user))
+      UserSession.create(user)
+      @controller.stubs(:current_user).returns(user)
+    end
+
+    #def admin_role
+    #  role_by_name Role::ADMIN
+    #end
+    #
+    #def role_by_name name
+    #  Role.find_by_name name
+    #end
+  end
+end
